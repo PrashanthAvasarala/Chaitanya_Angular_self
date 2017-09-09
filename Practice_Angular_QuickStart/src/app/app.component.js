7b71f440b63d83cc265a50b0550b5f9bf9a0c28d
avasarala786 <prashanth.avasarala@gmail.com>
2017-09-09 04:04:11 -0400

    See README.md

--- Practice_Angular_QuickStart/src/app/app.component.js
+++ Practice_Angular_QuickStart/src/app/app.component.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
@@ -16,13 +19,20 @@ AppComponent = __decorate([
         selector: 'my-app',
         //template: `<h1>Welcome {{name}}</h1>`,
         templateUrl: './HTML/app.component.html',
-        styleUrls: ['./HTML/app.component.css'],
+        styleUrls: ['./HTML/app.component.css', 'styles.css'],
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
 var HomeComponent = (function () {
+    //Just like Java constructor is invoke first as when the component is created
+    // In ECMA script 6 when write function() {body} as () => {} also
     function HomeComponent() {
+        var _this = this;
         this.name = ' ';
+        this.buttonClick = false;
+        setTimeout(function () {
+            _this.buttonClick = true;
+        }, 5000);
     }
     return HomeComponent;
 }());
@@ -30,8 +40,13 @@ HomeComponent = __decorate([
     core_1.Component({
         selector: 'my-home',
         templateUrl: './HTML/app.home.html',
+        /*  template : `<br>
+         <input type = "text" [(ngModel)] = "name"/>
+         <br><br>
+         <p> Welcome {{name}}</p>`, */
         styleUrls: ['./HTML/app.component.css'],
-    })
+    }),
+    __metadata("design:paramtypes", [])
 ], HomeComponent);
 exports.HomeComponent = HomeComponent;
 //# sourceMappingURL=app.component.js.map
--- Practice_Angular_QuickStart/src/app/app.component.ts
+++ Practice_Angular_QuickStart/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   //template: `<h1>Welcome {{name}}</h1>`,
   templateUrl : './HTML/app.component.html',
-  styleUrls : ['./HTML/app.component.css'],
+  styleUrls : ['./HTML/app.component.css','styles.css'],
 })
 export class AppComponent { 
    
@@ -12,11 +12,24 @@ export class AppComponent {
 
  @Component({
   selector: 'my-home',
-  templateUrl : './HTML/app.home.html',
+  templateUrl : './HTML/app.home.html', // we can write three to four lines of HTML code in template as well
+ /*  template : `<br>
+  <input type = "text" [(ngModel)] = "name"/>
+  <br><br>
+  <p> Welcome {{name}}</p>`, */
   styleUrls : ['./HTML/app.component.css'],
   
 })
 
 export class HomeComponent{
    name:any = ' ';
+   buttonClick = false;
+
+  //Just like Java constructor is invoke first as when the component is created
+  // In ECMA script 6 when write function() {body} as () => {} also
+   constructor() {
+         setTimeout(() => {
+          this.buttonClick = true;
+                    },5000)
+   }
 }
